f49577741aca9bc9d43b80e4b6ceadebaaffbeb1
Dhiraj Basukala <dhiraj.basukala@gmail.com>
2025-10-31 11:03:13 +1100

    add confirm plugin to use pretterd as formatter for speed

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      html = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
+      scss = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      vue = { "prettierd", "prettier", stop_after_first = true }, -- Add this
+      json = { "prettierd", "prettier", stop_after_first = true },
+      yaml = { "prettierd", "prettier", stop_after_first = true },
+      markdown = { "prettierd", "prettier", stop_after_first = true },
+      go = { "gofmt", "goimports" },
+    },
+  },
+}
